7015a73ca944e2e73984ae92999dd7ab5201917e
liugang <liugang@yunmaohulian.com>
2022-02-14 23:45:01 +0800

    init pep-iot

---
+++ src/main/java/com/example/pep/iot/PepIotApplication.java
@@ -0,0 +1,13 @@
+package com.example.pep.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepIotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PepIotApplication.class, args);
+    }
+
+}
